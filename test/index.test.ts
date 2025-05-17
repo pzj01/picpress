efb4f2b8dd9b778fcf371f79ada010736def183d
pzj01 <2949195453@qq.com>
2025-05-17 10:34:20 +0800

    chore: add test case

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,35 @@
-import { it } from 'vitest'
+import { statSync } from 'node:fs'
+import { describe, expect, it } from 'vitest'
+import { PicPress } from '../src/picpress'
 
-it('test', () => {})
+describe('picPress', {
+  timeout: Infinity,
+  concurrent: true,
+}, () => {
+  const pic = new PicPress({
+    entry: '.image',
+    minFileSize: 0,
+    recursive: true,
+  })
+
+  it('compress', async () => {
+    const sizes = pic.paths.map((path) => {
+      const { size } = statSync(path)
+      return size
+    })
+    const results = await pic.compress()
+
+    results.forEach(({ size }, i) => {
+      expect(size).toBeLessThanOrEqual(sizes[i])
+    })
+  })
+
+  it('transform', async () => {
+    const results = await pic.transform({
+      targetFormat: 'webp',
+    })
+    results.forEach(({ format }) => {
+      expect(format).toBe('webp')
+    })
+  })
+})
